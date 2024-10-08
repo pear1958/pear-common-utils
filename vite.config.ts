4d9ce1b5f7082bdd5a9403c5a99a1830c6c96a8f
zhangzy <doxoops@163.com>
2024-10-08 23:39:53 +0800

    feat: files

--- vite.config.ts
+++ vite.config.ts
@@ -6,32 +6,33 @@ export default defineConfig({
   plugins: [
     dts({
       outDir: ['./dist/es', './dist/lib'],
-      copyDtsFiles: true
-    })
+      copyDtsFiles: true,
+    }),
   ],
   build: {
     minify: false,
     lib: {
       entry: './index.ts',
       // 通过cdn引入后的全局变量名称
-      name: 'PearTools'
+      name: 'PearCommonUtils',
     },
     rollupOptions: {
+      external: ['jszip'],
       input: ['index.ts'],
       output: [
         {
           format: 'es',
           entryFileNames: '[name].js',
           preserveModules: true,
-          dir: resolve(__dirname, './dist/es') // 配置打包根目录
+          dir: resolve(__dirname, './dist/es'), // 配置打包根目录
         },
         {
           format: 'cjs',
           entryFileNames: '[name].js',
           preserveModules: true,
-          dir: resolve(__dirname, './dist/lib')
-        }
-      ]
-    }
-  }
+          dir: resolve(__dirname, './dist/lib'),
+        },
+      ],
+    },
+  },
 })
