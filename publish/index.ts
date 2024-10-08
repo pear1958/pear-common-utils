032b4858ce8412c772239bdb62f178bf9a4610a8
zhangzy <zhangzhiyong@klicen.com>
2024-10-08 14:17:54 +0800

    fix: script

--- publish/index.ts
+++ publish/index.ts
@@ -1,9 +1,12 @@
 import { series } from 'gulp'
+import { resolve } from 'path'
 
 import run from './run'
 
+const rootPath = resolve(__dirname, '../')
+
 export const publishUtils = async () => {
-  run('release-it', '../dist')
+  run('release-it', `${rootPath}/dist`)
 }
 
 export default series(async () => publishUtils())
