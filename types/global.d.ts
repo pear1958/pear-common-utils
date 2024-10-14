563d4614c519995870d8a98607b67de49b3d8d1a
zhangzy <zhangzhiyong@klicen.com>
2024-10-14 13:51:12 +0800

    fix: 优化代码

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,13 +1,13 @@
-export declare type Recordable<T = any> = Record<string, T>
+export type Recordable<T = any> = Record<string, T>
 
-export declare type Nullable<T> = T | null
+export type Nullable<T> = T | null
 
-export declare type TargetContext = '_self' | '_blank'
+export type TargetContext = '_self' | '_blank'
 
-export declare interface LabelValue<T = string | number> {
+export interface LabelValue<T = string | number> {
   label: string
   value: T
   [key: string]: any
 }
 
-export declare type TimeoutId = NodeJS.Timeout | string | number | undefined
+export type TimeoutId = NodeJS.Timeout | string | number | undefined
